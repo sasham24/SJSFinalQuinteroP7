c0edb69d388e5ea1167e3ceb333d4a1f32f16a9f
sasham24 <736149@student.dusd.net>
2024-05-22 15:25:52 -0700

    vroom
    added movement to one car and camera switcher

--- P7FinalSJS/Assets/Scripts/PlayerController.cs
+++ P7FinalSJS/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    public string inputID;
     
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    private float forwardInput;
+    private float horizontalInput;
+    private float turnSpeed = 100.0f;
+    private float speed = 25.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+        // Moves the car forward based on vertical input
+
+        // Moves the care forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
+  
