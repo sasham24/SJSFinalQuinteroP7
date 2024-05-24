1d11dc15bb1e9886844c95a2f9521032685db3c4
sasham24 <736149@student.dusd.net>
2024-05-24 14:56:52 -0700

    added 2 cameras

---
+++ P7FinalSJS/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    public string inputID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private float forwardInput;
+    private float horizontalInput;
+    private float turnSpeed = 100.0f;
+    private float speed = 25.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+        // Moves the car forward based on vertical input
+
+        // Moves the care forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+    }
+}
